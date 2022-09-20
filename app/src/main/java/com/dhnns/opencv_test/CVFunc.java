f73389be7ac10df4e1dee61db0cea204db962718
77elon <88618717+77elon@users.noreply.github.com>
2022-09-20 15:46:16 +0900

    OpenCV + NCNN
    텐서플로우를 사용하지 않기로 했다.

--- app/src/main/java/com/dhnns/opencv_test/CVFunc.java
+++
@@ -1,9 +0,0 @@
-package com.dhnns.opencv_test;
-
-public class CVFunc {
-    public static native void ConvertRGBtoGray(long matAddrInput, long matAddrResult);
-
-    public static native void PCACompute(long src_);
-
-    public static native void flite_test(long src_);
-}
