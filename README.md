fe01c47106a6da1dc11eb53dd02f9a232871c06f
77elon <88618717+77elon@users.noreply.github.com>
2022-09-20 15:28:23 +0900

    CMake OpenCV 적용
    tensorflow lite 모델 적용을 위해, 하나의 프로젝트에서 2개의 CMake 빌드 적용시도

---
+++ README.md
@@ -0,0 +1,2 @@
+# CaMelShot
+ 2022-1 캡스톤, OpenCV
---
+++ app/src/main/java/com/dhnns/opencv_test/CVFunc.java
@@ -0,0 +1,9 @@
+package com.dhnns.opencv_test;
+
+public class CVFunc {
+    public static native void ConvertRGBtoGray(long matAddrInput, long matAddrResult);
+
+    public static native void PCACompute(long src_);
+
+    public static native void flite_test(long src_);
+}
